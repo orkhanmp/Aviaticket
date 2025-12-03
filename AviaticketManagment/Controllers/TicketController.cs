610ce21e84876a06a2ebcd58dd03e02f54d9ecd5
orkhanmp <orkhanmansurov43@gmail.com>
2025-12-03 23:03:41 +0400

    mvc

---
+++ AviaticketManagment/Controllers/TicketController.cs
@@ -0,0 +1,59 @@
+﻿using AviaticketManagment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace AviaticketManagment.Controllers
+{
+    public class TicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Ticket> tickets = new List<Ticket>
+            {
+                new Ticket
+                {
+                    Type = "Premium",
+                    Country = "Italy",
+                    City = "Roma",
+                    FlightDate = DateTime.Now.AddDays(5),
+                    Price = 1200,
+                    BaggageKg = 40,
+                    Meal = true,
+                    SeatSelection = true,
+                    Wifi = true,
+                    FastCheckIn = true
+                },
+
+                new Ticket
+                {
+                    Type = "Standard",
+                    Country = "Turkey",
+                    City = "İstanbul",
+                    FlightDate = DateTime.Now.AddDays(10),
+                    Price = 700,
+                    BaggageKg = 35,
+                    Meal = true,
+                    SeatSelection = true,
+                    Wifi = false,
+                    FastCheckIn = false
+                },
+
+                new Ticket
+                {
+                    Type = "Economy",
+                    Country = "Georgia",
+                    City = "Tbilisi",
+                    FlightDate = DateTime.Now.AddDays(3),
+                    Price = 400,
+                    BaggageKg = 30,
+                    Meal = false,
+                    SeatSelection = false,
+                    Wifi = false,
+                    FastCheckIn = false
+                }
+
+            };
+            return View(tickets);
+        }
+    }
+}
---
+++ AviaticketManagment/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿namespace AviaticketManagment.Models
+{
+    public class Ticket
+    {
+        public string Type { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public DateTime FlightDate { get; set; }
+        public decimal Price { get; set; }
+        public int BaggageKg { get; set; }
+        public bool Meal { get; set; }
+        public bool SeatSelection { get; set; }
+        public bool Wifi { get; set; }
+        public bool FastCheckIn { get; set; }
+    }
+}
